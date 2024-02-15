a7d9e1f11b5bbc149a9c77023b4504f44459cce5
Eriber Tejeda <59887012+eriber01@users.noreply.github.com>
2024-02-15 11:21:46 -0400

    feat: update image stream 2024-02-15

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -15,27 +15,42 @@ interface returnImage {
   success: boolean
 }
 
+interface resImg {
+  public_id: string
+  secure_url: string
+}
+
 export async function updateImage(file: File, folder: string): Promise<returnImage> {
   try {
 
     const bytes = await file.arrayBuffer()
     const buffer = Buffer.from(bytes)
 
-    const filepath = path.join(process.cwd(), 'public', file.name)
+    // const filepath = path.join(process.cwd(), 'public', file.name)
 
-    await writeFileSync(filepath, buffer)
+    // await writeFileSync(filepath, buffer)
 
-    const res = await cloudinary.uploader.upload(filepath, {
-      folder: `newPortfolio/${folder}`,
-      use_filename: true
+    // const res = await cloudinary.uploader.upload(filepath, {
+    //   folder: `newPortfolio/${folder}`,
+    //   use_filename: true
+    // })
+
+    const res: any = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload_stream({
+        folder: `newPortfolio/${folder}`,
+        use_filename: true
+      }, (err, result) => {
+        if (err) reject(err)
+        resolve(result)
+      }).end(buffer)
     })
 
-    await fs.unlink(filepath, error => {
-      if (error) {
-        console.log('error image: ', error);
-        throw new Error("Error delete the image")
-      }
-    })
+    // await fs.unlink(filepath, error => {
+    //   if (error) {
+    //     console.log('error image: ', error);
+    //     throw new Error("Error delete the image")
+    //   }
+    // })
 
     return {
       publicId: res.public_id,
