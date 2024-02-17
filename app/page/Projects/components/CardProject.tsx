82c0c1cee14a872e78617e25a20eea13c2cf00d5
Eriber Tejeda <59887012+eriber01@users.noreply.github.com>
2024-02-17 14:46:22 -0400

    feat: add get data in home and about page 2024-02-16

--- app/page/Projects/components/CardProject.tsx
+++ app/page/Projects/components/CardProject.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-import Image from "next/image"
-import TechImages from "../../Abouts/components/TechImages"
-import { getTech } from "../../Abouts/database"
 import CardButtons from "./CardButtons"
 
 import style from '../style.module.css'
