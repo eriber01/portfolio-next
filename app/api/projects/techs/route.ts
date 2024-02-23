edd5f7a2b9729e125cfc16810dba7ca3b949c630
Eriber Tejeda <59887012+eriber01@users.noreply.github.com>
2024-02-22 20:14:36 -0400

    feat: auth and login page ready 2024-02-22

--- app/api/projects/techs/route.ts
+++ app/api/projects/techs/route.ts
@@ -5,7 +5,7 @@ export async function GET(_: Request) {
 
   try {
 
-    const techs = await prisma?.techs.findMany({ where: { OR: [{ show_type: 2 }, { show_type: 3 }] }, include: { image: true } })
+    const techs = await prisma.techs.findMany({ where: { OR: [{ show_type: 2 }, { show_type: 3 }], enabled: true }, include: { image: true } })
 
     return NextResponse.json({ message: '', status: 'success', techs })
   } catch (error) {
